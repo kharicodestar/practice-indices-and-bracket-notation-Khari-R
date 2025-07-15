d6c9f8b6cb478a9408188d3a82e560e4f142b47c
Khari <kharicodestar@gmail.com>
2025-07-15 11:45:23 -0500

    Created 2D array with 9 fictional books.

---
+++ indices.js
@@ -0,0 +1,12 @@
+const readline = require('readline-sync');
+
+
+let bookSeries = [
+    ["Harry Potter", "Twilight", "Hunger Games"],
+    ["Eragon", "The Lord of the Rings", "Mistborn"],
+    ["The Chronicles of Narnia", "Game of Thrones", "The Maze Runner"]
+
+];
+
+console.log(bookSeries);
+console.log(bookSeries[1]);
